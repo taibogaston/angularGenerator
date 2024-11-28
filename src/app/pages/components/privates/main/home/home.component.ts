60f7078e065291bfb4b16d4f972eb67f684563c3
gtaibo <gtaibo@encodelabs.tech>
2024-11-28 16:27:44 -0300

    master

--- src/app/pages/components/privates/main/home/home.component.ts
+++ src/app/pages/components/privates/main/home/home.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CardModule} from 'primeng/card';
 import {PrincipalTableExampleComponent} from '../principal-table-example/principal-table-example.component';
 import {CalendarModule} from 'primeng/calendar';
 import {FormsModule} from '@angular/forms';
+import {DOCUMENT} from '@angular/common';
+import {InputSwitchModule} from 'primeng/inputswitch';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +14,27 @@ import {FormsModule} from '@angular/forms';
     CardModule,
     PrincipalTableExampleComponent,
     CalendarModule,
-    FormsModule
+    FormsModule,
+    InputSwitchModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   date: Date[] | undefined;
+  #document = inject(DOCUMENT);
+  isDarkMode = false;
+
+  toggleLightDark() {
+    const linkElement = this.#document.getElementById('theme-css') as HTMLLinkElement;
+    if (linkElement.href.includes('light')) {
+      linkElement.href = 'assets/styles/theme/lara-dark-indigo/theme.css';
+      this.isDarkMode = true;
+    } else {
+      linkElement.href = 'assets/styles/theme/lara-light-indigo/theme.css';
+      this.isDarkMode = false;
+    }
+  }
+
+
 }
